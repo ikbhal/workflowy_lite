2613d77ecc158250ee7ee44b8ae642f90d0c9da8
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-22 14:49:33 +0530

    commit, add button to enter

--- static/app.js
+++ static/app.js
@@ -1,97 +1,84 @@
 Vue.component('checklist-item', {
-    props: ['item', 'itemIndex'],
-    data() {
-      return {
-        showPopup: false
-      };
-    },
-    template: `
-      <li class="list-group-item" :class="{ 'checked': item.checked }" @mouseover="showPopup = true" @mouseleave="showPopup = false">
-        <span @click="toggleCollapse" class="collapse-icon" v-if="item.children.length > 0" :class="{ 'expand': !item.collapsed, 'collapse': item.collapsed }"></span>
-        <span @click="toggleCheck" class="check-icon">{{ item.checked ? '\u2713' : '\u2714' }}</span>
-        <span>{{ item.text }}</span>
-        <span @click="deleteItem" class="text-danger">Delete</span>
-        <div v-if="showPopup" class="popup-menu">
-          <span @click="deleteItem">Delete</span>
-        </div>
-        <ul v-if="item.children.length > 0 && !item.collapsed" class="list-group mt-2">
-          <checklist-item v-for="(childItem, index) in item.children" :key="index" :item="childItem" :itemIndex="index"></checklist-item>
-        </ul>
-      </li>
-    `,
-    methods: {
-      toggleCheck() {
-        this.item.checked = !this.item.checked;
-        this.saveChecklistToLocalStorage();
-      },
-      toggleCollapse() {
-        this.item.collapsed = !this.item.collapsed;
-        this.saveChecklistToLocalStorage();
-      },
-      deleteItem() {
-        const index = this.$parent.checklistData.indexOf(this.item);
-        if (index !== -1) {
-          this.$parent.checklistData.splice(index, 1);
-          this.saveChecklistToLocalStorage();
-        }
-      },
-      saveChecklistToLocalStorage() {
-        localStorage.setItem('checklist', JSON.stringify(this.$parent.checklistData));
-      }
-    }
-  });
-  
-  const app = new Vue({
-    el: '#app',
-    data: {
+  props: ['item'],
+  data() {
+    return {
+      showSubMenu: false,
       newItemText: '',
-      checklistData: JSON.parse(localStorage.getItem('checklist')) || []
+      sublistCollapsed: false // New data property to track sublist collapse state
+    };
+  },
+  template: `
+    <li class="list-group-item">
+      <span @click="toggleCollapse" class="collapse-icon" v-if="item.children.length > 0" :class="{ 'expand': !item.collapsed, 'collapse': item.collapsed }"></span>
+      <span>{{ item.text }}</span>
+      <span @click="deleteItem" class="text-danger">Delete</span>
+      <span @click="toggleSubMenu" class="add-sublist-icon" v-if="!item.collapsed && item.children.length === 0">+</span>
+      <div v-if="showSubMenu" class="sublist-input">
+        <input v-model="newItemText" @keyup.enter="addSublistItem" type="text" class="form-control" placeholder="Add new item">
+        <button @click="addSublistItem" class="btn btn-primary btn-sm">Add</button>
+      </div>
+      <span v-if="item.children.length > 0" @click="toggleSublistCollapse" class="sublist-collapse-icon" :class="{ 'sublist-expand': !sublistCollapsed, 'sublist-collapse': sublistCollapsed }"></span>
+      <ul v-if="item.children.length > 0 && (!item.collapsed || !sublistCollapsed)" class="list-group mt-2">
+        <checklist-item v-for="(childItem, index) in item.children" :key="index" :item="childItem"></checklist-item>
+      </ul>
+    </li>
+  `,
+  methods: {
+    toggleCollapse() {
+      this.item.collapsed = !this.item.collapsed;
     },
-    methods: {
-      addItem() {
-        const trimmedText = this.newItemText.trim();
-        if (trimmedText !== '') {
-          this.createItem(trimmedText);
-          this.newItemText = '';
-        }
-      },
-      createItem(text, parent = null) {
-        const newItem = {
-          text,
-          checked: false,
-          collapsed: false,
-          children: []
-        };
-  
-        if (parent === null) {
-          this.checklistData.push(newItem);
-        } else {
-          parent.children.push(newItem);
-        }
-  
-        this.saveChecklistToLocalStorage();
-      },
-      saveChecklistToLocalStorage() {
-        localStorage.setItem('checklist', JSON.stringify(this.checklistData));
+    toggleSubMenu() {
+      this.showSubMenu = !this.showSubMenu;
+    },
+    deleteItem() {
+      const index = this.$parent.checklistData.indexOf(this.item);
+      if (index !== -1) {
+        this.$parent.checklistData.splice(index, 1);
       }
     },
-    watch: {
-      checklistData: {
-        deep: true,
-        handler() {
-          this.saveChecklistToLocalStorage();
-        }
+    addSublistItem() {
+      const trimmedText = this.newItemText.trim();
+      if (trimmedText !== '') {
+        this.item.children.push({
+          text: trimmedText,
+          children: [],
+          collapsed: false
+        });
+        this.newItemText = '';
       }
     },
-    mounted() {
-      const sortableList = this.$refs.sortableList;
-      Sortable.create(sortableList, {
-        animation: 150,
-        onUpdate: (evt) => {
-          const movedItem = this.checklistData.splice(evt.oldIndex, 1)[0];
-          this.checklistData.splice(evt.newIndex, 0, movedItem);
-        }
-      });
+    toggleSublistCollapse() {
+      this.sublistCollapsed = !this.sublistCollapsed;
     }
-  });
-  
+  }
+});
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    newItemText: '',
+    checklistData: []
+  },
+  methods: {
+    addItem() {
+      const trimmedText = this.newItemText.trim();
+      if (trimmedText !== '') {
+        this.checklistData.push({
+          text: trimmedText,
+          children: [],
+          collapsed: false
+        });
+        this.newItemText = '';
+      }
+    }
+  },
+  mounted() {
+    // Listen for the Enter key press event on the input element
+    const inputElement = document.getElementById('newItemInput');
+    inputElement.addEventListener('keyup', (event) => {
+      if (event.key === 'Enter') {
+        this.addItem();
+      }
+    });
+  }
+});

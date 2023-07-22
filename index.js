1e5230bd19a9ab6679d8653af70cae415d4045fa
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-22 16:35:00 +0530

    3004

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3006;
+const port = 3004;
 
 //static attach to express
 app.use(express.static(path.join(__dirname, 'static')));

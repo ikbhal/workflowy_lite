901970dc3965127a02006feac7ee6fa86489ccbb
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-22 16:19:35 +0530

    commit

--- static/app.js
+++ static/app.js
@@ -4,31 +4,38 @@ Vue.component('checklist-item', {
     return {
       showSubMenu: false,
       newItemText: '',
-      sublistCollapsed: false // New data property to track sublist collapse state
+      sublistCollapsed: false,
+      showNewItemText: false
     };
   },
   template: `
     <li class="list-group-item">
-      <span @click="toggleCollapse" class="collapse-icon" v-if="item.children.length > 0" :class="{ 'expand': !item.collapsed, 'collapse': item.collapsed }"></span>
+      <span v-if="item.children.length > 0" 
+        @click="toggleCollapse"
+         class="sublist-collapse-icon"
+        :class="{ 'sublist-expand': !sublistCollapsed, 'sublist-collapse': sublistCollapsed }"></span>
       <span>{{ item.text }}</span>
-      <span @click="deleteItem" class="text-danger">Delete</span>
       <span @click="toggleSubMenu" class="add-sublist-icon" v-if="!item.collapsed && item.children.length === 0">+</span>
-      <div v-if="showSubMenu" class="sublist-input">
+      <div v-if="showNewItemText" class="sublist-input">
         <input v-model="newItemText" @keyup.enter="addSublistItem" type="text" class="form-control" placeholder="Add new item">
-        <button @click="addSublistItem" class="btn btn-primary btn-sm">Add</button>
       </div>
-      <span v-if="item.children.length > 0" @click="toggleSublistCollapse" class="sublist-collapse-icon" :class="{ 'sublist-expand': !sublistCollapsed, 'sublist-collapse': sublistCollapsed }"></span>
+      
       <ul v-if="item.children.length > 0 && (!item.collapsed || !sublistCollapsed)" class="list-group mt-2">
         <checklist-item v-for="(childItem, index) in item.children" :key="index" :item="childItem"></checklist-item>
       </ul>
     </li>
   `,
   methods: {
-    toggleCollapse() {
-      this.item.collapsed = !this.item.collapsed;
-    },
+    // toggleCollapse() {
+    //   this.item.collapsed = !this.item.collapsed;
+    // },
     toggleSubMenu() {
       this.showSubMenu = !this.showSubMenu;
+      if (this.showSubMenu) {
+        this.$nextTick(() => {
+          this.$refs.addInput.focus();
+        });
+      }
     },
     deleteItem() {
       const index = this.$parent.checklistData.indexOf(this.item);
@@ -47,8 +54,12 @@ Vue.component('checklist-item', {
         this.newItemText = '';
       }
     },
-    toggleSublistCollapse() {
+    toggleCollapse() {
       this.sublistCollapsed = !this.sublistCollapsed;
+      this.item.collapsed = !this.item.collapsed;
+    },
+    toggleInput() {
+      this.showNewItemText = !this.showNewItemText;
     }
   }
 });
@@ -57,7 +68,8 @@ const app = new Vue({
   el: '#app',
   data: {
     newItemText: '',
-    checklistData: []
+    checklistData: [],
+    showAddInput: false
   },
   methods: {
     addItem() {
@@ -73,7 +85,6 @@ const app = new Vue({
     }
   },
   mounted() {
-    // Listen for the Enter key press event on the input element
     const inputElement = document.getElementById('newItemInput');
     inputElement.addEventListener('keyup', (event) => {
       if (event.key === 'Enter') {
